aa99e27d8cf69247dfddf2ef2c1f64e34d91fa11
Benson Chen <tumlivein@gmail.com>
2020-12-17 22:04:08 +0800

    加入 Redux Devtools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,8 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { addUser, getUsers } from './actions/users';
 import usersReducer from './reducers/users';
 
-const store = createStore(usersReducer);
+const store = createStore(usersReducer, composeWithDevTools());
 
 export default store;
