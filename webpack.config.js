ab449a12760cab4ad766f6b281ebf318649a5a0f
Benson Chen <tumlivein@gmail.com>
2020-12-17 21:14:27 +0800

    環境設定

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const { resolve } = require('path');
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const config = {
+    entry: {
+        main: resolve('./src/index.tsx'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: ['awesome-typescript-loader?module=es6'],
+                exclude: [/node_modules/],
+            },
+            {
+                test: /\.js$/,
+                loader: 'source-map-loader',
+                enforce: 'pre',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+    ],
+};
+
+module.exports = config;
