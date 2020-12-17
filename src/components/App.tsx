533705f495d9fe3f54906b3a6f08b0774f0f71d1
Benson Chen <tumlivein@gmail.com>
2020-12-17 21:51:00 +0800

    連結 React 和 Redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Users from './Users';
+import { Provider } from 'react-redux';
+import store from '@redux/store';
 
 const App = () => {
-    return <Users />;
+    return (
+        <Provider store={store}>
+            <Users />
+        </Provider>
+    );
 };
 
 export default App;
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,30 +1,49 @@
 import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Card from '@components/Card';
+import { getUsers } from '@redux/actions/users';
+import { I_User } from '@redux/reducers/users';
 
-const tempUsers = [
+const tempUsers: I_User[] = [
     {
         id: 1,
-        name: 'Yooa',
-        company: {
-            name: 'WM',
-            catchPhrase: 'find it ~ Oh My Girl',
-        },
+        name: '白熊',
+        company: { name: '角落生物' },
     },
     {
         id: 2,
-        name: 'Binnie',
-        company: {
-            name: 'WM',
-            catchPhrase: 'find it ~ Oh My Girl',
-        },
+        name: '企鵝?',
+        company: { name: '角落生物' },
+    },
+    {
+        id: 3,
+        name: '炸豬排',
+        company: { name: '角落生物' },
+    },
+    {
+        id: 4,
+        name: '貓',
+        company: { name: '角落生物' },
+    },
+    {
+        id: 5,
+        name: '蜥蜴',
+        company: { name: '角落生物' },
     },
 ];
 
 const Users = () => {
+    const users = useSelector((state: I_User[]) => state);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getUsers(tempUsers));
+    }, []);
+
     return (
         <div className="users">
-            {tempUsers.length > 0 ? (
-                tempUsers.map((user) => <Card key={user.id} user={user} />)
+            {users.length > 0 ? (
+                users.map((user) => <Card key={user.id} user={user} />)
             ) : (
                 <p>No Users Available</p>
             )}
